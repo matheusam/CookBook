f450a49e3326dfd45b3e6e463615190f2cb383fd
Henrique Morato <henrique.morato@campuscode.com.br>
2019-01-03 14:00:02 -0200

    Visitante vê detalhes de uma receita

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_155042) do
+ActiveRecord::Schema.define(version: 2019_01_03_155631) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_01_03_155042) do
     t.integer "cook_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ingredients"
+    t.text "cook_method"
   end
 
 end
