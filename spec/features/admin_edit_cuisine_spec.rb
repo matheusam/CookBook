585494e793d814e9e64e47a62e353cff25478d76
Matheus Acosta Martins <matheusthebr@gmail.com>
2019-01-20 19:14:10 -0200

    Validações da edição de receitas.

--- spec/features/admin_edit_cuisine_spec.rb
+++ spec/features/admin_edit_cuisine_spec.rb
@@ -12,4 +12,32 @@ feature 'Admin edit cuisine' do
 
     expect(page).to have_css('h1', text:'Brasileira')
   end
+
+  scenario 'and must fill in name',login:true do
+    cuisine = Cuisine.create(name: 'Basileila')
+
+    visit root_path
+    click_on 'Cozinhas'
+    click_on cuisine.name
+    click_on 'Editar'
+    fill_in 'Renomear', with: ''
+    click_on 'Salvar'
+
+    expect(page).to have_content('Você deve informar o nome da cozinha')
+  end
+
+  scenario 'Unique',login:true do
+
+    wrong_cuisine = Cuisine.create(name: 'Basileila')
+    cuisine = Cuisine.create(name: 'Brasileira')
+
+    visit root_path
+    click_on 'Cozinhas'
+    click_on wrong_cuisine.name
+    click_on 'Editar'
+    fill_in 'Renomear', with: cuisine.name.upcase
+    click_on 'Salvar'
+
+    expect(page).to have_content('Essa cozinha já está cadastrada!')
+  end
 end
