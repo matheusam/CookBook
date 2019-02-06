df543c64a69a809d3e3af585964a67e6a781f4ee
Matheus Acosta Martins <matheusthebr@gmail.com>
2019-02-06 19:34:14 -0200

    Ajustes schema

--- db/migrate/20190124223825_create_recipe_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecipeLists < ActiveRecord::Migration[5.2]
-  def change
-    create_table :recipe_lists do |t|
-      t.belongs_to :recipe, index: true
-      t.belongs_to :list, index: true
-      t.timestamps
-    end
-  end
-end
