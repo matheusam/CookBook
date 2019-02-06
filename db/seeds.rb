83fff11dfe9d0e818c4af32365946bef505cdf80
Matheus Acosta Martins <matheusthebr@gmail.com>
2019-02-06 20:28:52 -0200

    Ajustes nos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,28 @@
-RecipeType.create(name: 'Sobremesa')
-RecipeType.create(name: 'Entrada')
+# recipe types
+RecipeType.create(name: 'Acompanhamentos')
+RecipeType.create(name: 'Bebidas')
+main_dish = RecipeType.create(name: 'Prato Principal')
+dessert = RecipeType.create(name: 'Sobremesa')
+
+# cuisines
 Cuisine.create(name: 'Portuguesa')
 brazilian_cuisine = Cuisine.create(name: 'Brasileira')
-italian_cuisine = Cuisine.create(name: 'Italiana')
-recipe_type = RecipeType.create(name: 'Prato Principal')
-Recipe.create(title: 'Feijoada', recipe_type: recipe_type,
+german_cuisine = Cuisine.create(name: 'Alemã')
+
+# users
+user = User.create(email:'user@email.com', password: '12345678')
+other_user = User.create(email:'other_user@email.com', password: '12345678')
+
+# recipes
+Recipe.create(title: 'Feijoada', recipe_type: main_dish,
               cuisine: brazilian_cuisine,
               difficulty: 'Médio', cook_time: 120,
               ingredients: 'Feijão e carnes.',
-              cook_method: 'Misturar o feijão com as carnes.')
+              cook_method: 'Misturar o feijão com as carnes.', user: user)
 
-Recipe.create(title: 'Macarronada', recipe_type: recipe_type,
-              cuisine: italian_cuisine,
+Recipe.create(title: 'Strudel', recipe_type: dessert,
+              cuisine: german_cuisine,
               difficulty: 'Fácil', cook_time: 30,
-              ingredients: 'Macarrão e molho de tomate',
-              cook_method: 'Cozinhar o macarrão e misturar com o molho.')
+              ingredients: 'Maça, uvas passas, massa folheada',
+              cook_method: 'Cozinhar a maçã e colocar na massa com uva passas.',
+              user: other_user)
